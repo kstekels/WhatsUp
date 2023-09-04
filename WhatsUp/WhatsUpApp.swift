25fd0ef8e70f011cfb2d4ae492b53acd39c1613e
kstekels <kstekels+github@gmail.com>
2023-09-04 15:29:34 +0300

    Initial Commit

---
+++ WhatsUp/WhatsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsUpApp.swift
+//  WhatsUp
+//
+//  Created by Kārlis Štekels on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhatsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
